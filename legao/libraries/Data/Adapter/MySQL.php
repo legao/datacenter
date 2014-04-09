21a7c1c801ff3b7d2ca2ee62f6867caacde9f25e
wanglong <mail@wanglong.name>
2014-04-09 15:31:17 +0800

    添加link注释信息

--- legao/libraries/Data/Adapter/MySQL.php
+++ legao/libraries/Data/Adapter/MySQL.php
@@ -12,6 +12,7 @@
  * @author      Wang Kuang, Wang Long
  * @copyright   Copyright (c) 2014 - 2015 , All rights reserved.
  * @license     http://opensource.org/licenses/OSL-3.0
+ * @link        https://github.com/legao/datacenter
  */
 namespace Legao\Data\Adapter;
 
